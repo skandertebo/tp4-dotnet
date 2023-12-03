b01b0d715f8e657cb0891b15dec6e32f409a0d7b
skandertebo <alexandertebourb@gmail.com>
2023-12-03 19:08:06 +0100

    init commit

---
+++ TP3/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TP3.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        [ForeignKey("GenresId")]
+        public int? GenresId { get; set; }
+
+        //Navigation Property
+        public virtual Genre?  Genres { get; set; }
+        public List<Customer>? Customers { get; set; }  
+        
+        //add attributes
+
+        public DateTime? MovieAdded { get; set; }
+        public string? Photo { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+
+       
+
+
+    }
+}
---
+++ TP3/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using TP3.Models;
+using TP4.Services.Services;
+using TP4.Services.ServiceContracts;
+using TP4.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+builder.Services.AddScoped<IMovieService, MovieService>();
+
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
